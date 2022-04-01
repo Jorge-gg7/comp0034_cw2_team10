48c542f08b4ad5e302281a52085ce23ecabbe996
Brandon <ekhai_chew@yahoo.com>
2022-04-01 11:55:29 +0100

    added delete function

--- flask_app/main/routes.py
+++ flask_app/main/routes.py
@@ -29,3 +29,41 @@ def new_post():
         flash('Your post has been created!', 'success')
         return redirect(url_for('main_bp.home'))
     return render_template('create_post.html', title='New Post', form=form)
+
+
+@main_bp.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@main_bp.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('main_bp.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@main_bp.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main_bp.home'))
